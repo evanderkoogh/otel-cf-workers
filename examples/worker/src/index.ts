b004d693c4f6818e02c74e778202aee13606b93a
Erwin van der Koogh <git@koogh.com>
2023-04-02 01:39:41 +1100

    add example of adding attribute to a current span

--- examples/worker/src/index.ts
+++ examples/worker/src/index.ts
@@ -1,4 +1,5 @@
-import { instrument } from '../../../src/index'
+import { trace } from '@opentelemetry/api'
+import { instrument, WorkerTraceConfig } from '../../../src/index'
 
 export interface Env {
 	OTEL_TEST: KVNamespace
@@ -9,12 +10,15 @@ const handler = {
 		await fetch('https://cloudflare.com')
 		await env.OTEL_TEST.get('non-existant')
 		const greeting = "G'day World"
+		trace.getActiveSpan()?.setAttribute('greeting', greeting)
 		return new Response(`${greeting}!`)
 	},
 }
 
-export default instrument(handler, {
+const config: WorkerTraceConfig = {
 	exporter: { url: 'https://api.honeycomb.io/v1/traces' },
 	serviceName: 'greetings',
 	serviceVersion: '0.1',
-})
+}
+
+export default instrument(handler, config)
