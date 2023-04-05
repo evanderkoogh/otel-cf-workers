b23283f9af2a804bca63347a884e0e6172de60b7
Erwin van der Koogh <git@koogh.com>
2023-04-05 18:37:37 +1000

    rework caching logic to fix Proxy issues

--- src/instrumentation/cache.ts
+++ src/instrumentation/cache.ts
@@ -1,57 +1,61 @@
 import { trace } from '@opentelemetry/api'
 import { WorkerTraceConfig } from '../config'
-import { sanitiseURL, wrap } from './common'
+import { isWrapped, sanitiseURL, unwrap, wrap } from './common'
 
 type CacheFns = Cache[keyof Cache]
 
 const tracer = trace.getTracer('cache instrumentation')
 
-function instrumentFunction<T extends CacheFns>(fn: T, cacheName: string, config: WorkerTraceConfig): T {
-	return new Proxy(fn, {
+function instrumentFunction<T extends CacheFns>(fn: T, cacheName: string, op: string, _config: WorkerTraceConfig): T {
+	const handler: ProxyHandler<typeof fn> = {
 		apply(target, thisArg, argArray) {
-			return tracer.startActiveSpan(`cache:${cacheName}:${fn.name}`, (span) => {
+			return tracer.startActiveSpan(`cache:${cacheName}:${op}`, async (span) => {
 				span.setAttribute('cache.name', cacheName)
 				if (argArray[0].url) {
 					span.setAttribute('http.url', sanitiseURL(argArray[0].url))
 				}
-				const result = Reflect.apply(target, thisArg, argArray)
+				const result = await Reflect.apply(target, thisArg, argArray)
 				span.end()
 				return result
 			})
 		},
-	})
+	}
+	return wrap(fn, handler)
 }
 
 function instrumentCache(cache: Cache, cacheName: string, config: WorkerTraceConfig): Cache {
-	return new Proxy(cache, {
+	const handler: ProxyHandler<typeof cache> = {
 		get(target, prop) {
-			const fn = Reflect.get(target, prop)
-			return instrumentFunction(fn, cacheName, config)
+			if (prop === 'delete' || prop === 'match' || prop === 'put') {
+				const fn = Reflect.get(target, prop).bind(target)
+				return instrumentFunction(fn, cacheName, prop, config)
+			} else {
+				return Reflect.get(target, prop)
+			}
 		},
-	})
+	}
+	return wrap(cache, handler)
 }
 
 function instrumentOpen(openFn: CacheStorage['open'], config: WorkerTraceConfig): CacheStorage['open'] {
-	return new Proxy(openFn, {
+	const handler: ProxyHandler<typeof openFn> = {
 		async apply(target, thisArg, argArray) {
 			const cacheName = argArray[0]
-			return tracer.startActiveSpan(`cache:open:${cacheName}`, async (span) => {
-				const cache = await Reflect.apply(target, thisArg, argArray)
-				span.end()
-				return instrumentCache(cache, cacheName, config)
-			})
+			const cache = await Reflect.apply(target, thisArg, argArray)
+			return instrumentCache(cache, cacheName, config)
 		},
-	})
+	}
+	return wrap(openFn, handler)
 }
 
 export function instrumentGlobalCache(config: WorkerTraceConfig) {
 	const handler: ProxyHandler<typeof caches> = {
 		get(target, prop) {
 			if (prop === 'default') {
-				const cache = Reflect.get(target, prop)
+				const cache = target.default
 				return instrumentCache(cache, 'default', config)
 			} else if (prop === 'open') {
-				const openFn = Reflect.get(target, prop)
+				const openFn = Reflect.get(target, prop).bind(target)
 				return instrumentOpen(openFn, config)
 			} else {
 				return Reflect.get(target, prop)
