e547b1c1e00c956fbf3fb37108a4a0bf3f928067
Erwin van der Koogh <git@koogh.com>
2023-04-07 12:47:23 +1000

    Refactor: Move instrumentation of global fetch and caches to a globals.ts file.

--- src/instrumentation/fetch.ts
+++ src/instrumentation/fetch.ts
@@ -2,24 +2,10 @@ import { trace, SpanOptions, SpanKind, propagation, context, Attributes, Excepti
 import { SpanProcessor } from '@opentelemetry/sdk-trace-base'
 import { SemanticAttributes } from '@opentelemetry/semantic-conventions'
 import { loadConfig, init, PartialTraceConfig } from '../config'
-import { WorkerTraceConfig } from '../config'
+import { gatherRequestAttributes, gatherResponseAttributes } from './common'
 import { instrumentEnv } from './env'
-import { sanitiseURL, wrap } from './common'
 
 type FetchHandler<E, C> = ExportedHandlerFetchHandler<E, C>
-
-const gatherRequestAttributes = (request: Request): Attributes => {
-	const attrs: Record<string, string | number> = {}
-	const headers = request.headers
-	// attrs[SemanticAttributes.HTTP_CLIENT_IP] = '1.1.1.1'
-	attrs[SemanticAttributes.HTTP_METHOD] = request.method
-	attrs[SemanticAttributes.HTTP_URL] = sanitiseURL(request.url)
-	attrs[SemanticAttributes.HTTP_USER_AGENT] = headers.get('user-agent')!
-	attrs[SemanticAttributes.HTTP_REQUEST_CONTENT_LENGTH] = headers.get('content-length')!
-	attrs['http.request_content-type'] = headers.get('content-type')!
-	attrs['http.accepts'] = headers.get('accepts')!
-	return attrs
-}
 
 const gatherIncomingCfAttributes = (request: Request): Attributes => {
 	const attrs: Record<string, string | number> = {}
@@ -31,27 +17,6 @@ const gatherIncomingCfAttributes = (request: Request): Attributes => {
 	attrs['net.tls_version'] = request.cf?.tlsVersion as string
 	attrs['net.asn'] = request.cf?.asn as number
 	attrs['net.tcp_rtt'] = request.cf?.clientTcpRtt as number
-	return attrs
-}
-
-const gatherOutgoingCfAttributes = (cf: RequestInitCfProperties): Attributes => {
-	const attrs: Record<string, string | number> = {}
-	Object.keys(cf).forEach((key) => {
-		const value = cf[key]
-		if (typeof value === 'string' || typeof value === 'number') {
-			attrs[`cf.${key}`] = value
-		} else {
-			attrs[`cf.${key}`] = JSON.stringify(value)
-		}
-	})
-	return attrs
-}
-
-const gatherResponseAttributes = (response: Response): Attributes => {
-	const attrs: Record<string, string | number> = {}
-	attrs[SemanticAttributes.HTTP_STATUS_CODE] = response.status
-	attrs[SemanticAttributes.HTTP_RESPONSE_CONTENT_LENGTH] = response.headers.get('content-length')!
-	attrs['http.response_content-type'] = response.headers.get('content-type')!
 	return attrs
 }
 
@@ -164,29 +129,4 @@ const instrumentFetchHandler = <E, C>(
 	})
 }
 
-const instrumentGlobalFetch = (_config: PartialTraceConfig): void => {
-	const handler: ProxyHandler<typeof fetch> = {
-		apply: (target, thisArg, argArray): ReturnType<typeof fetch> => {
-			const tracer = trace.getTracer('fetch')
-			const options: SpanOptions = { kind: SpanKind.CLIENT }
-
-			const request = new Request(argArray[0], argArray[1])
-			const host = new URL(request.url).host
-			const promise = tracer.startActiveSpan(`fetch: ${host}`, options, async (span) => {
-				propagation.inject(context.active(), request.headers, {
-					set: (h, k, v) => h.set(k, typeof v === 'string' ? v : String(v)),
-				})
-				span.setAttributes(gatherRequestAttributes(request))
-				if (request.cf) span.setAttributes(gatherOutgoingCfAttributes(request.cf))
-				const response: Response = await Reflect.apply(target, thisArg, [request])
-				span.setAttributes(gatherResponseAttributes(response))
-				span.end()
-				return response
-			})
-			return promise
-		},
-	}
-	globalThis.fetch = wrap(fetch, handler)
-}
-
-export { instrumentGlobalFetch, instrumentFetchHandler }
+export { instrumentFetchHandler }
