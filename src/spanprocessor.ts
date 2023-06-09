55f14fe67f220fdaa09fc152bae6db8a3acad592
Erwin van der Koogh <git@koogh.com>
2023-06-09 12:26:04 +1000

    No longer need the FlushOnlySpanProcessor

--- src/spanprocessor.ts
+++ src/spanprocessor.ts
@@ -98,34 +98,3 @@ export class BatchTraceSpanProcessor implements SpanProcessor {
 	async forceFlush(): Promise<void> {}
 	async shutdown(): Promise<void> {}
 }
-
-export class FlushOnlySpanProcessor implements SpanProcessor {
-	readonly exporter: SpanExporter
-	readonly readableSpans: ReadableSpan[] = []
-
-	constructor(exporter: SpanExporter) {
-		this.exporter = exporter
-	}
-
-	forceFlush(): Promise<void> {
-		return new Promise((resolve, reject) => {
-			const spliced = this.readableSpans.splice(0)
-			if (spliced.length > 0) {
-				this.exporter.export(spliced, (result) => {
-					if (result.code === ExportResultCode.SUCCESS) {
-						resolve()
-					} else {
-						reject(result.error)
-					}
-				})
-			}
-		})
-	}
-
-	onStart(_span: Span, _parentContext: Context): void {}
-
-	onEnd(span: ReadableSpan): void {
-		this.readableSpans.push(span)
-	}
-	async shutdown(): Promise<void> {}
-}
