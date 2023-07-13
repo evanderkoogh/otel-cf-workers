310a8c6cbc195c8ca46f0afab6247eca3d0168b2
Tyler Benfield <rtbenfield@gmail.com>
2023-07-13 16:40:18 -0400

    fixed includeTraceParent resolving

--- src/instrumentation/fetch.ts
+++ src/instrumentation/fetch.ts
@@ -174,7 +174,11 @@ export function instrumentFetcher(
 			const host = new URL(request.url).host
 			const spanName = typeof attrs?.['name'] === 'string' ? attrs?.['name'] : `fetch: ${host}`
 			const promise = tracer.startActiveSpan(spanName, options, async (span) => {
-				if (config.includeTraceContext) {
+				const includeTraceContext =
+					typeof config.includeTraceContext === 'function'
+						? config.includeTraceContext(request)
+						: config.includeTraceContext
+				if (includeTraceContext ?? true) {
 					propagation.inject(api_context.active(), request.headers, {
 						set: (h, k, v) => h.set(k, typeof v === 'string' ? v : String(v)),
 					})
