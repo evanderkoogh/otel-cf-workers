15a5daa7d574da7a7c68450a680111e2021d842a
Dániel Földi <dani@stefan.hu>
2024-05-04 14:17:12 +0200

    Add back service binding fetch instrumentation

--- src/instrumentation/env.ts
+++ src/instrumentation/env.ts
@@ -22,11 +22,6 @@ const isDurableObject = (item?: unknown): item is DurableObjectNamespace => {
 	return !isJSRPC(item) && !!(item as DurableObjectNamespace)?.idFromName
 }
 
-const isServiceBinding = (item?: unknown): item is Fetcher => {
-	const binding = item as Fetcher
-	return (!isJSRPC(item) && !!binding.connect) || !!binding.fetch || binding.queue || binding.scheduled
-}
-
 export const isVersionMetadata = (item?: unknown): item is WorkerVersionMetadata => {
 	return (
 		!isJSRPC(item) &&
@@ -47,16 +42,13 @@ const instrumentEnv = (env: Record<string, unknown>): Record<string, unknown> =>
 				return item
 			}
 			if (isJSRPC(item)) {
-				// TODO instrument JSRPC and maybe remove serviceBinding?
-				return item
+				return instrumentServiceBinding(item, String(prop))
 			} else if (isKVNamespace(item)) {
 				return instrumentKV(item, String(prop))
 			} else if (isQueue(item)) {
 				return instrumentQueueSender(item, String(prop))
 			} else if (isDurableObject(item)) {
 				return instrumentDOBinding(item, String(prop))
-			} else if (isServiceBinding(item)) {
-				return instrumentServiceBinding(item, String(prop))
 			} else if (isVersionMetadata(item)) {
 				// we do not need to log accesses to the metadata
 				return item
