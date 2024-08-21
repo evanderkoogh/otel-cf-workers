evanderkoogh/otel-cf-workers

commit bd0a46a69da163ded5a6412b5bc839db159dd8f9

Upgrade dependencies and make @open-telemetry/api a peer-dependency

src/exporter.ts
@@ -43,7 +43,7 @@ export class OTLPExporter implements SpanExporter {
 	}
 
 	send(items: any[], onSuccess: () => void, onError: (error: OTLPExporterError) => void): void {
-		const exportMessage = createExportTraceServiceRequest(items, true)
+		const exportMessage = createExportTraceServiceRequest(items)
 		const body = JSON.stringify(exportMessage)
 		const params: RequestInit = {
 			method: 'POST',

src/instrumentation/fetch.ts
@@ -121,7 +121,7 @@ function getParentContextFromRequest(request: Request) {
 	const acceptTraceContext =
 		typeof workerConfig.handlers.fetch.acceptTraceContext === 'function'
 			? workerConfig.handlers.fetch.acceptTraceContext(request)
-			: workerConfig.handlers.fetch.acceptTraceContext ?? true
+			: (workerConfig.handlers.fetch.acceptTraceContext ?? true)
 	return acceptTraceContext ? getParentContextFromHeaders(request.headers) : api_context.active()
 }
 
@@ -156,22 +156,19 @@ export function executeFetchHandler(fetchFn: FetchHandler, [request, env, ctx]: 
 	const promise = tracer.startActiveSpan(`fetchHandler ${method}`, options, spanContext, async (span) => {
 		const readable = span as unknown as ReadableSpan
 		try {
-			const response: Response = await fetchFn(request, env, ctx)
+			const response = await fetchFn(request, env, ctx)
 			span.setAttributes(gatherResponseAttributes(response))
-			if (readable.attributes['http.route']) {
-				span.updateName(`fetchHandler ${method} ${readable.attributes['http.route']}`)
-			}
-			span.end()
 
 			return response
 		} catch (error) {
+			span.recordException(error as Exception)
+			span.setStatus({ code: SpanStatusCode.ERROR })
+			throw error
+		} finally {
 			if (readable.attributes['http.route']) {
 				span.updateName(`fetchHandler ${method} ${readable.attributes['http.route']}`)
 			}
-			span.recordException(error as Exception)
-			span.setStatus({ code: SpanStatusCode.ERROR })
 			span.end()
-			throw error
 		}
 	})
 	return promise
@@ -205,8 +202,8 @@ export function instrumentClientFetch(
 	configFn: getFetchConfig,
 	attrs?: Attributes,
 ): Fetcher['fetch'] {
-	const handler: ProxyHandler<typeof fetch> = {
-		apply: (target, thisArg, argArray): ReturnType<typeof fetch> => {
+	const handler: ProxyHandler<Fetcher['fetch']> = {
+		apply: (target, thisArg, argArray): Response | Promise<Response> => {
 			const request = new Request(argArray[0], argArray[1])
 			if (!request.url.startsWith('http')) {
 				return Reflect.apply(target, thisArg, argArray)
@@ -236,7 +233,7 @@ export function instrumentClientFetch(
 				}
 				span.setAttributes(gatherRequestAttributes(request))
 				if (request.cf) span.setAttributes(gatherOutgoingCfAttributes(request.cf))
-				const response: Response = await Reflect.apply(target, thisArg, [request])
+				const response = await Reflect.apply(target, thisArg, [request])
 				span.setAttributes(gatherResponseAttributes(response))
 				span.end()
 				return response
@@ -248,5 +245,6 @@ export function instrumentClientFetch(
 }
 
 export function instrumentGlobalFetch(): void {
+	//@ts-ignore For some reason the node types are imported and complain.
 	globalThis.fetch = instrumentClientFetch(globalThis.fetch, (config) => config.fetch)
 }

src/span.ts
@@ -112,6 +112,15 @@ export class SpanImpl implements Span, ReadableSpan {
 		this.onEnd = init.onEnd
 	}
 
+	addLink(link: Link): this {
+		this.links.push(link)
+		return this
+	}
+	addLinks(links: Link[]): this {
+		this.links.push(...links)
+		return this
+	}
+
 	spanContext(): SpanContext {
 		return this._spanContext
 	}

commit bd81d403ef89fc3ecf662a49e46cca35edbf9992

fix: otlp json encoding
Regression introduced in commit https://github.com/evanderkoogh/otel-cf-workers/commit/bd0a46a69da163ded5a6412b5bc839db159dd8f9 after api change in https://github.com/open-telemetry/opentelemetry-js/pull/4220

src/exporter.ts
@@ -43,7 +43,10 @@ export class OTLPExporter implements SpanExporter {
 	}
 
 	send(items: any[], onSuccess: () => void, onError: (error: OTLPExporterError) => void): void {
-		const exportMessage = createExportTraceServiceRequest(items)
+		const exportMessage = createExportTraceServiceRequest(items, {
+			useHex: true,
+			useLongBits: false,
+		})
 		const body = JSON.stringify(exportMessage)
 		const params: RequestInit = {
 			method: 'POST',