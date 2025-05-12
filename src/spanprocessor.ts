8e5c5ac5d570bdc657cdd0af5dbade67e06096bd
Erwin van der Koogh <890386+evanderkoogh@users.noreply.github.com>
2025-05-12 13:42:41 +1000

    Rework SpanProcessor to also work without external flush
    This is needed for things like Durable Objects which do not mandate something like Context.waitUntil to continue running after the request is returned.

--- src/spanprocessor.ts
+++ src/spanprocessor.ts
@@ -1,48 +1,93 @@
 import { Context, Span } from '@opentelemetry/api'
 import { ReadableSpan, SpanExporter } from '@opentelemetry/sdk-trace-base'
 import { ExportResultCode } from '@opentelemetry/core'
-import { getActiveConfig } from './config'
+//import { getActiveConfig } from './config'
 import { TraceFlushableSpanProcessor } from './types'
 
+class TraceState {
+	private unexportedSpans: ReadableSpan[] = []
+	private inprogressSpans = new Set<string>()
+	private exporter: SpanExporter
+	private exportPromises: Promise<void>[] = []
+	// private traceDecision?: boolean
+
+	constructor(exporter: SpanExporter) {
+		this.exporter = exporter
+	}
+
+	addSpan(span: Span): void {
+		this.unexportedSpans.push(span as unknown as ReadableSpan)
+		this.inprogressSpans.add(span.spanContext().spanId)
+	}
+
+	endSpan(span: ReadableSpan): void {
+		this.inprogressSpans.delete(span.spanContext().spanId)
+		if (this.inprogressSpans.size === 0) {
+			this.flush()
+		}
+	}
+
+	async flush(): Promise<void> {
+		//Check/do sampling
+		if (this.unexportedSpans.length > 0) {
+			const finishedSpans = this.unexportedSpans.filter((span) => !this.inprogressSpans.has(span.spanContext().spanId))
+			this.unexportedSpans = this.unexportedSpans.filter((span) => this.inprogressSpans.has(span.spanContext().spanId))
+			if (finishedSpans.length > 0) {
+				this.exportPromises.push(this.exportSpans(finishedSpans))
+			}
+		}
+		await Promise.allSettled(this.exportPromises)
+	}
+
+	private async exportSpans(spans: ReadableSpan[]): Promise<void> {
+		await scheduler.wait(1)
+		const promise = new Promise<void>((resolve, reject) => {
+			this.exporter.export(spans, (result) => {
+				if (result.code === ExportResultCode.SUCCESS) {
+					console.log('Done exporting.')
+					resolve()
+				} else {
+					console.log('exporting spans failed! ' + result.error)
+					reject(result.error)
+				}
+			})
+		})
+		await promise
+	}
+}
+
+type traceId = string
 export class BatchTraceSpanProcessor implements TraceFlushableSpanProcessor {
-	private traces: Record<string, Span[]> = {}
+	private traces: Record<traceId, TraceState> = {}
 
 	constructor(private exporter: SpanExporter) {}
 
-	private export(traceId: string): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			const config = getActiveConfig()
-			if (!config) throw new Error('Config is undefined. This is a bug in the instrumentation logic')
-			const spans = this.traces[traceId] as unknown[] as ReadableSpan[] | undefined
-			if (spans) {
-				this.exporter.export(spans, (result) => {
-					if (result.code === ExportResultCode.SUCCESS) {
-						resolve()
-					} else {
-						console.log('exporting spans failed! ' + result.error)
-						reject(result.error)
-					}
-				})
-			} else {
-				resolve()
-			}
-		})
+	getTraceState(traceId: string): TraceState {
+		const traceState = this.traces[traceId] || new TraceState(this.exporter)
+		this.traces[traceId] = traceState
+		return traceState
 	}
 
 	onStart(span: Span, _parentContext: Context): void {
 		const traceId = span.spanContext().traceId
-		const spans = this.traces[traceId] || []
-		spans.push(span)
-		this.traces[traceId] = spans
+		this.getTraceState(traceId).addSpan(span)
 	}
 
-	onEnd(_span: ReadableSpan): void {
-		//this space intentionally left blank
+	onEnd(span: ReadableSpan): void {
+		const traceId = span.spanContext().traceId
+		this.getTraceState(traceId).endSpan(span)
 	}
 
-	forceFlush(traceId: string = ''): Promise<void> {
-		return this.export(traceId)
+	async forceFlush(traceId?: traceId): Promise<void> {
+		if (traceId) {
+			await this.getTraceState(traceId).flush()
+		} else {
+			const promises = Object.values(this.traces).map((traceState: TraceState) => traceState.flush)
+			await Promise.allSettled(promises)
+		}
 	}
 
-	async shutdown(): Promise<void> {}
+	async shutdown(): Promise<void> {
+		await this.forceFlush()
+	}
 }
