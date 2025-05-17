fa75f0c3b6a3b75757e686fbd6c7546855a46f4e
Erwin van der Koogh <890386+evanderkoogh@users.noreply.github.com>
2025-05-17 14:46:04 +1000

    Create new example for new style Durable Objects

---
+++ examples/new_style-do/src/handler.ts
@@ -0,0 +1,23 @@
+import { DurableObject } from 'cloudflare:workers';
+
+export class MyDurableObject extends DurableObject<Env> {
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+	}
+
+	async sayHello(name: string): Promise<string> {
+		return `Hello, ${name}!`;
+	}
+}
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName('foo');
+
+		const stub = env.MY_DURABLE_OBJECT.get(id);
+
+		const greeting = await stub.sayHello('world');
+
+		return new Response(greeting);
+	},
+} satisfies ExportedHandler<Env>;
